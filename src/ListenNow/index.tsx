1820ffe8b7f85b0c6166ddf027342fd9f8cc5850
Jiahao Lu <lujjjh@gmail.com>
2021-09-23 18:00:26 +0800

    Remove unsupported recommendation types

--- src/ListenNow/index.tsx
+++ src/ListenNow/index.tsx
@@ -20,13 +20,14 @@ const ListenNow = () => {
     const offset = new Date().getTimezoneOffset(),
       o = Math.abs(offset)
     const tz = (offset < 0 ? '+' : '-') + ('00' + Math.floor(o / 60)).slice(-2) + ':' + ('00' + (o % 60)).slice(-2)
+    const qs = new URLSearchParams()
+    qs.set('timezone', tz)
+    qs.set('platform', 'web')
+    qs.set('name', 'listen-now')
+    qs.set('types', 'artists,albums,playlists,stations')
     const {
       data: { data },
-    } = await instance.api.music(
-      `v1/me/recommendations?timezone=${encodeURIComponent(
-        tz
-      )}&with=friendsMix%2Clibrary&name=listen-now&art%5Burl%5D=c%2Cf&omit%5Bresource%5D=autos&relate%5Beditorial-items%5D=contents&extend=editorialCard%2CeditorialVideo&extend%5Balbums%5D=artistUrl&extend%5Bplaylists%5D=artistNames%2CeditorialArtwork&extend%5Bsocial-profiles%5D=topGenreNames&include%5Balbums%5D=artists&include%5Bsongs%5D=artists&include%5Bmusic-videos%5D=artists&fields%5Balbums%5D=artistName%2CartistUrl%2Cartwork%2CcontentRating%2CeditorialArtwork%2CeditorialVideo%2Cname%2CplayParams%2CreleaseDate%2Curl&fields%5Bartists%5D=name%2Curl&extend%5Bstations%5D=airDate%2CsupportsAirTimeUpdates&meta%5Bstations%5D=inflectionPoints&types=artists%2Calbums%2Ceditorial-items%2Clibrary-albums%2Clibrary-playlists%2Cmusic-movies%2Cmusic-videos%2Cplaylists%2Cstations%2Cuploaded-audios%2Cuploaded-videos%2Cactivities%2Capple-curators%2Ccurators%2Ctv-shows&relate=catalog&platform=web`
-    )
+    } = await instance.api.music(`v1/me/recommendations?${qs.toString()}`)
     return data
   })
   return (
