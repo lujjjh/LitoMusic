4d209f701f2f6b84c54921ec6b1affc06387051e
Jiahao Lu <lujjjh@gmail.com>
2021-09-24 12:29:58 +0800

    Set authorized state on next tick

--- src/useAuthorized.ts
+++ src/useAuthorized.ts
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 
 const useAuthorized = () => {
-  const [authorized, setAuthorized] = useState<boolean>(MusicKit.getInstance().isAuthorized)
+  const [authorized, setAuthorized] = useState<boolean>(false)
   useEffect(() => {
     const instance = MusicKit.getInstance()
     const handleAuthorizationStatusDidChange = () => {
-      setAuthorized(instance.isAuthorized)
+      // set on the next tick as mediaUserToken might be not ready
+      setTimeout(() => {
+        setAuthorized(instance.isAuthorized)
+      }, 0)
     }
+    handleAuthorizationStatusDidChange()
     instance.addEventListener(MusicKit.Events.authorizationStatusDidChange, handleAuthorizationStatusDidChange)
     return () => {
       instance.removeEventListener(MusicKit.Events.authorizationStatusDidChange, handleAuthorizationStatusDidChange)
