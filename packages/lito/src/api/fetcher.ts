8792defe4bb1a2326c9a6f2cda2dd97fdb5b36ec
Jiahao Lu <lujjjh@gmail.com>
2021-10-21 14:45:28 +0800

    Unauthorize when access denied

--- packages/lito/src/api/fetcher.ts
+++ packages/lito/src/api/fetcher.ts
@@ -1,8 +1,16 @@
 export const fetcher = async (url: string) => {
+  const instance = MusicKit.getInstance()
   const {
     api,
     _bag: { store },
-  } = MusicKit.getInstance() as any
-  const { data } = await api.music(url)
-  return store.populateDataRecords(data)
+  } = instance as any
+  try {
+    const { data } = await api.music(url)
+    return store.populateDataRecords(data)
+  } catch (error) {
+    if (error instanceof MusicKit.MKError && error.errorCode === MusicKit.MKError.ACCESS_DENIED) {
+      instance.unauthorize()
+    }
+    throw error
+  }
 }
